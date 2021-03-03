f41dbe5d274985c67412b0d89139910132601431
Kulbhushan <kulbhushan.mayer@>
2021-03-03 10:08:44 +0530

    added pip and nic steps

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "azurerm_resource_group" "kul-rg" {
 # terraform validate
 # terraform plan
 # terraform apply
+# terraform apply -auto-approve
 
 resource "azurerm_virtual_network" "kul-vnet" {
   name = "kul-net"
@@ -26,4 +27,24 @@ resource "azurerm_subnet" "default" {
   address_prefixes = [ "10.0.1.0/24" ]
   virtual_network_name = azurerm_virtual_network.kul-vnet.name
   resource_group_name = azurerm_resource_group.kul-rg.name
+}
+
+resource "azurerm_public_ip" "kul-pip" {
+  name = "kul-pip"
+  location = azurerm_resource_group.kul-rg.location
+  resource_group_name = azurerm_resource_group.kul-rg.name
+  allocation_method = "Dynamic"
+  sku = "Basic"
+}
+
+resource "azurerm_network_interface" "kul-nic" {
+  name = "kul-nic"
+  location = azurerm_resource_group.kul-rg.location
+  resource_group_name = azurerm_resource_group.kul-rg.name
+  ip_configuration {
+    name = "ipconfig"
+    subnet_id = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.kul-pip.id
+  }
 }
